fc9d09f8f9c04dbea5ca574c8036ccafcf7fff15
Gangadhar N <gangadharn1299@gmail.com>
2020-07-29 22:21:00 +0530

    Updates

---
+++ precache-manifest.8a39add26766f5b2d6021644e83a590c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65808db1160434baf80cadb0e39369cc",
+    "url": "/smart-brain-app-reactjs/index.html"
+  },
+  {
+    "revision": "29d154b41de6181dd245",
+    "url": "/smart-brain-app-reactjs/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c2ccc5714e6922b17d1e",
+    "url": "/smart-brain-app-reactjs/static/css/main.56a41fb2.chunk.css"
+  },
+  {
+    "revision": "29d154b41de6181dd245",
+    "url": "/smart-brain-app-reactjs/static/js/2.ed4b0634.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart-brain-app-reactjs/static/js/2.ed4b0634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2ccc5714e6922b17d1e",
+    "url": "/smart-brain-app-reactjs/static/js/main.b0b2885f.chunk.js"
+  },
+  {
+    "revision": "1346f06b288a88448ad9",
+    "url": "/smart-brain-app-reactjs/static/js/runtime-main.d7b04ca0.js"
+  }
+]);
